349cb576104389b61a0fb2c93eb856164b19e648
Lecostarius <aingram1@hotmail.com>
2019-03-22 20:11:47 +0100

    added entire code to main.c from Waveshare demo pgm, WORKS

--- Src/main.c
+++ Src/main.c
@@ -100,7 +100,104 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
+	if(EPD_Init(lut_full_update) != 0) {
+        printf("e-Paper init failed\r\n");
+    }
+    EPD_Clear();
+    DEV_Delay_ms(500);
 
+    //Create a new image cache
+    UBYTE *BlackImage;		
+		/* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
+    UWORD Imagesize = ((EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1)) * EPD_HEIGHT;
+    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+				return 0;
+    }
+    printf("Paint_NewImage\r\n");
+		
+    Paint_NewImage(BlackImage, EPD_WIDTH, EPD_HEIGHT, 270, WHITE);
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);
+		#if 1   //show image for array    
+    printf("show image for array\r\n");
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);
+    Paint_DrawBitMap(gImage_1in54);
+    
+    EPD_Display(BlackImage);
+    DEV_Delay_ms(2000);
+#endif
+
+#if 1   // Drawing on the image
+    //1.Select Image
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);
+
+    // 2.Drawing on the image
+    Paint_DrawPoint(5, 10, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+    Paint_DrawPoint(5, 25, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+    Paint_DrawPoint(5, 40, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+    Paint_DrawPoint(5, 55, BLACK, DOT_PIXEL_4X4, DOT_STYLE_DFT);
+
+    Paint_DrawLine(20, 10, 70, 60, BLACK, LINE_STYLE_SOLID, DOT_PIXEL_1X1);
+    Paint_DrawLine(70, 10, 20, 60, BLACK, LINE_STYLE_SOLID, DOT_PIXEL_1X1);
+    Paint_DrawLine(170, 15, 170, 55, BLACK, LINE_STYLE_DOTTED, DOT_PIXEL_1X1);
+    Paint_DrawLine(150, 35, 190, 35, BLACK, LINE_STYLE_DOTTED, DOT_PIXEL_1X1);
+
+    Paint_DrawRectangle(20, 10, 70, 60, BLACK, DRAW_FILL_EMPTY, DOT_PIXEL_1X1);
+    Paint_DrawRectangle(85, 10, 130, 60, BLACK, DRAW_FILL_FULL, DOT_PIXEL_1X1);
+
+    Paint_DrawCircle(170, 35, 20, BLACK, DRAW_FILL_EMPTY, DOT_PIXEL_1X1);
+    Paint_DrawCircle(170, 85, 20, BLACK, DRAW_FILL_FULL, DOT_PIXEL_1X1);
+    Paint_DrawString_EN(5, 85, "waveshare", &Font20, BLACK, WHITE);
+    Paint_DrawNum(5, 110, 123456789, &Font20, BLACK, WHITE);
+
+    Paint_DrawString_CN(5, 135,"���abc", &Font12CN, BLACK, WHITE);
+    Paint_DrawString_CN(5, 160, "΢ѩ����", &Font24CN, WHITE, BLACK);
+
+    EPD_Display(BlackImage);
+    DEV_Delay_ms(2000);
+#endif
+
+#if 1   //Partial refresh, example shows time    
+    if(EPD_Init(lut_partial_update) != 0) {
+        printf("e-Paper init failed\r\n");
+    }
+		Paint_SelectImage(BlackImage);
+    PAINT_TIME sPaint_time;
+    sPaint_time.Hour = 12;
+    sPaint_time.Min = 34;
+    sPaint_time.Sec = 56;
+    for (;;) {
+        sPaint_time.Sec = sPaint_time.Sec + 1;
+        if (sPaint_time.Sec == 60) {
+            sPaint_time.Min = sPaint_time.Min + 1;
+            sPaint_time.Sec = 0;
+            if (sPaint_time.Min == 60) {
+                sPaint_time.Hour =  sPaint_time.Hour + 1;
+                sPaint_time.Min = 0;
+                if (sPaint_time.Hour == 24) {
+                    sPaint_time.Hour = 0;
+                    sPaint_time.Min = 0;
+                    sPaint_time.Sec = 0;
+                }
+            }
+        }
+        Paint_ClearWindows(15, 65, 15 + Font20.Width * 7, 65 + Font20.Height, WHITE);
+        Paint_DrawTime(15, 65, &sPaint_time, &Font20, WHITE, BLACK);
+
+				EPD_Display(BlackImage);
+        DEV_Delay_ms(500);//Analog clock 1s
+    }
+
+#endif
+
+    printf("Goto Sleep mode...\r\n");
+    EPD_Sleep();
+    free(BlackImage);
+    BlackImage = NULL;
+    
   /* USER CODE END 2 */
 
   /* Infinite loop */
